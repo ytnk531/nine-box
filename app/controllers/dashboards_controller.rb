fe38a1ccf5d647b11edb150ef346018f2966fab8
ytnk531 <ytnk531@gmail.com>
2021-10-16 22:43:10 +0900

    解答

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,14 +1,14 @@
 class DashboardsController < ApplicationController
   def new
     state = Game.new.detect
-    render state.selector_view
+    render state.view(current_user)
   end
 
   def select
     state = Game.new.detect
-    state.next(params[:position].to_i)
+    state.next(params[:position].to_i, current_user)
     flash.now[:notice] = state.notice
-    render state.view
+    render state.view(current_user)
 end
 
   def create
